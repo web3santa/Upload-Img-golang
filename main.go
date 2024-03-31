47f0c4e1ef317fcf475a8bfb7b99fb53afbf9c5a
web3santa <usefulstory35@gmail.com>
2024-04-01 00:05:41 +0900

    finished

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/olahol/go-imageupload"
+)
+
+var currentImage *imageupload.Image
 
 func main() {
 
@@ -8,5 +15,43 @@ func main() {
 
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.File("index.html")
+
 	})
+
+	r.GET("/image", func(ctx *gin.Context) {
+		if currentImage == nil {
+			ctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		currentImage.Write(ctx.Writer)
+
+	})
+
+	r.GET("/thumbnail", func(ctx *gin.Context) {
+		if currentImage == nil {
+			ctx.AbortWithStatus(http.StatusNotFound)
+		}
+
+		t, err := imageupload.ThumbnailJPEG(currentImage, 150, 150, 1)
+		if err != nil {
+			panic(err)
+		}
+
+		t.Write(ctx.Writer)
+	})
+
+	r.POST("/upload", func(ctx *gin.Context) {
+		img, err := imageupload.Process(ctx.Request, "file")
+		if err != nil {
+			panic(err)
+		}
+
+		currentImage = img
+
+		ctx.Redirect(http.StatusMovedPermanently, "/")
+	})
+
+	r.Run(":3000")
+
 }
